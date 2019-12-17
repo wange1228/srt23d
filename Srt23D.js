b92420dbe68fd18af35e404b2c05967e8c93e637
万戈 <i@wange.im>
2019-12-17 15:52:08 +0800

    交互式传参

---
+++ Srt23D.js
@@ -0,0 +1,113 @@
+const fs = require('fs')
+const path = require('path')
+const iconv = require('iconv-lite')
+const jschardet = require('jschardet')
+
+class Srt23D {
+    // srtPath 字幕文件所在路径
+    // srtType = 'lr' 左右3D / srtType = 'td' 上下3D
+    // srtCallback 转换成功后的回调
+    constructor(srtPath, srtType = 'lr', srtCallback) {
+        this.init(srtPath, srtType, srtCallback)
+    }
+
+    // 初始化
+    init(srtPath, srtType, srtCallback) {
+        if (path.extname(srtPath) !== '.srt') { return }
+        const srtArray = this.getSrt(srtPath)
+        const newSrtContent = this.mergeSrt(srtArray, srtType)
+        const newSrtPath = this.setSrt(srtPath, newSrtContent)
+
+        return srtCallback({
+            oldPath: srtPath,
+            newPath: newSrtPath,
+            srtType: srtType
+        })
+    }
+
+    // 获取文件内容
+    getSrt(filePath) {
+        const srtBuf = fs.readFileSync(filePath)
+        // 编码检测并映射
+        let encoding = 'UTF8'
+        switch (jschardet.detect(srtBuf).encoding) {
+            case 'UTF-8':
+                encoding = 'UTF8'
+                break
+            case 'GB2312':
+                encoding = 'GB2312'
+                break
+        }
+        const srtData = iconv.decode(srtBuf, encoding).toString().split('\r\n').join('\n')
+        // 按空行分割
+        const srtSplit = srtData.split(/\n\n/)
+        const srtArray = []
+        srtSplit.forEach(val => {
+            const valSplit = val.split(/\n(\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3})\n/)
+            const time = valSplit[1]
+            const words = valSplit[2]
+            if (time && words) {
+                srtArray.push([ time, words ])
+            }
+        })
+
+        return srtArray
+    }
+
+    // 写文件内容
+    setSrt(filePath, fileContent) {
+        const newSrtPath = path.join(
+            path.dirname(filePath),
+            path.basename(filePath, '.srt') + '-3D' + '.srt'
+        )
+
+        fs.writeFileSync(newSrtPath, fileContent)
+
+        return newSrtPath
+    }
+
+    // 格式化内容
+    formatSrt(index, time, text, offset) {
+        const textSplit = text.split(/\n/)
+        let texts = ''
+        textSplit.forEach(val => {
+            if (val) {
+                texts += `${offset}${val}\r\n`
+            }
+        })
+
+        return `${index}\r\n${time}\r\n${texts}\r\n`
+    }
+
+    // 合并两屏内容
+    mergeSrt(data, type) {
+        let partA = ''
+        let partB = ''
+        let offsetA = ''
+        let offsetB = ''
+        let index = 0
+
+        switch (type) {
+            case 'lr':
+                offsetA = '{\\pos(96,255)\\fscx50}'
+                offsetB = '{\\pos(288,255)\\fscx50}'
+                break
+
+            case 'td':
+                offsetA = '{\\pos(192,268)\\fscy50}'
+                offsetB = '{\\pos(192,124)\\fscy50}'
+                break
+        }
+
+        data.forEach(val => {
+            partA += this.formatSrt(++index, val[0], val[1], offsetA)
+        })
+        data.forEach(val => {
+            partB += this.formatSrt(++index, val[0], val[1], offsetB)
+        })
+
+        return `${partA}${partB}`
+    }
+}
+
+module.exports = Srt23D
--- index.js
+++ index.js
@@ -1,113 +1,38 @@
 #!/usr/bin/env node
 
-// Example: node index.js ./xxx.srt
 const fs = require('fs')
 const path = require('path')
-const iconv = require('iconv-lite')
-const jschardet = require('jschardet')
+const inquirer = require('inquirer')
+const Srt23D = require('./Srt23D')
 
-class Srt23D {
-    // type = 'lr' 左右3D / type = 'td' 上下3D
-    constructor(filePath, type = 'lr') {
-        this.init(filePath, type)
-    }
-
-    // 初始化
-    init(filePath, type) {
-        if (path.extname(filePath) !== '.srt') { return }
-        const srtArray = this.getSrt(filePath)
-        const newSrtContent = this.mergeSrt(srtArray, type)
-        const newSrtPath = this.setSrt(filePath, newSrtContent)
-
-        return
-    }
-
-    // 获取文件内容
-    getSrt(filePath) {
-        const srtBuf = fs.readFileSync(filePath)
-        // 编码检测并映射
-        let encoding = 'UTF8'
-        switch (jschardet.detect(srtBuf).encoding) {
-            case 'UTF-8':
-                encoding = 'UTF8'
-                break
-            case 'GB2312':
-                encoding = 'GB2312'
-                break
+inquirer.prompt([{
+    type: 'input',
+    name: 'srtPath',
+    message: 'srt 字幕文件的路径',
+    validate: (input) => {
+        if (!fs.existsSync(input)) {
+            return '输入的文件路径不存在'
         }
-        const srtData = iconv.decode(srtBuf, encoding).toString().split('\r\n').join('\n')
-        // 按空行分割
-        const srtSplit = srtData.split(/\n\n/)
-        const srtArray = []
-        srtSplit.forEach(val => {
-            const valSplit = val.split(/\n(\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3})\n/)
-            const time = valSplit[1]
-            const words = valSplit[2]
-            if (time && words) {
-                srtArray.push([ time, words ])
-            }
-        })
-
-        return srtArray
-    }
-
-    // 写文件内容
-    setSrt(filePath, fileContent) {
-        const newSrtPath = path.join(
-            path.dirname(filePath),
-            path.basename(filePath, '.srt') + '-3D' + '.srt'
-        )
-
-        fs.writeFileSync(newSrtPath, fileContent)
-
-        return newSrtPath
-    }
-
-    // 格式化内容
-    formatSrt(index, time, text, offset) {
-        const textSplit = text.split(/\n/)
-        let texts = ''
-        textSplit.forEach(val => {
-            if (val) {
-                texts += `${offset}${val}\r\n`
-            }
-        })
-
-        return `${index}\r\n${time}\r\n${texts}\r\n`
-    }
-
-    // 合并两屏内容
-    mergeSrt(data, type) {
-        let partA = ''
-        let partB = ''
-        let offsetA = ''
-        let offsetB = ''
-        let index = 0
-
-        switch (type) {
-            case 'lr':
-                offsetA = '{\\pos(96,255)\\fscx50}'
-                offsetB = '{\\pos(288,255)\\fscx50}'
-                break
-
-            case 'td':
-                offsetA = '{\\pos(192,268)\\fscy50}'
-                offsetB = '{\\pos(192,124)\\fscy50}'
-                break
+        if (path.extname(input) !== '.srt') {
+            return '输入的文件路径不是 .srt 字幕格式'
         }
 
-        data.forEach(val => {
-            partA += this.formatSrt(++index, val[0], val[1], offsetA)
-        })
-        data.forEach(val => {
-            partB += this.formatSrt(++index, val[0], val[1], offsetB)
-        })
-
-        return `${partA}${partB}`
+        return true
     }
-}
+}, {
+    type: 'list',
+    name: 'srtType',
+    message: 'srt 字幕文件的类型',
+    choices: [{
+        name: '左右3D',
+        value: 'lr'
+    }, {
+        name: '上下3D',
+        value: 'td'
+    }]
+}]).then((answers) => {
+    new Srt23D(answers.srtPath, answers.srtType, (srt) => {
+        console.log(`转换已完成：${srt.newPath}`)
+    })
+})
 
-// 实例
-new Srt23D(
-    process.argv.splice(2)[0], 'lr'
-)
